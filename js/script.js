cdf96779d6d330994d99e861f41565f4dc27b042
Josie Dorsett <josie@MacBook-Pro-3.local>
2016-12-03 13:57:05 -0500

    message

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// for the science page, on click make image dissapear to text
+jQuery(document).ready(function() {
+  $('.imageclickA').click(function() {
+    $( ".clickedimageA" ).toggle( "slide" );
+  });
+});
+
+
+jQuery(document).ready(function() {
+  $('.imageclickB').click(function() {
+    $( ".clickedimageB" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickC').click(function() {
+    $( ".clickedimageC" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickD').click(function() {
+    $( ".clickedimageD" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickE').click(function() {
+    $( ".clickedimageE" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickE').click(function() {
+    $( ".clickedimageE" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickF').click(function() {
+    $( ".clickedimageF" ).toggle( "slide" );
+  });
+});
+
+jQuery(document).ready(function() {
+  $('.imageclickG').click(function() {
+    $( ".clickedimageG" ).toggle( "slide" );
+  });
+});
